751fcc88363ff58ceaeae48859541b8b6b6899e8
Lydia <lydia.boateng@student.moringaschool.com>
2022-06-22 11:48:45 +0100

    work done array method

--- index.js
+++ index.js
@@ -12,5 +12,11 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  const capTutorials = tutorials.map(t => {
+    const StubaWords = t.split(" ").map(w=> {return w.charAt(0).toUpperCase() + w.slice(1);
+      }).join(" ");
+      console.log (StubaWords);
+      return StubaWords;
+  })
+  return capTutorials
 }
